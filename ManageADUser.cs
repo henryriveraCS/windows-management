dc4bc15bbc06bd85b8017c0f3591893617aa22e6
Henry <henryrivera.cs@gmail.com>
2021-12-24 11:14:17 -0500

    LazyCreateADUser now works with exception catches.

--- ManageADUser.cs
+++ ManageADUser.cs
@@ -22,7 +22,6 @@ namespace SampleNamespace
 		private string _stackMsg = "";
 		private string _domainName = "";
 		private string _tld = "";
-		private string _fullName = "";
 		private string _username = "";
 		private string _upn = "";
 		//OU's pointing to base of AD + user 
@@ -41,6 +40,11 @@ namespace SampleNamespace
 			} else{
 				return "No error exception set.\n";
 			}
+		}
+
+		//Get stack messages at any point from your code
+		public string GetStackMessage(){
+			return _stackMsg;
 		}
 
 		//used to add values to the class stack message
@@ -144,19 +148,20 @@ namespace SampleNamespace
 			string fullPath = "LDAP://" + DomainName + "." + TLD;
 			AddStackMessage("Attempting to join directory: " + fullPath);
 			try{
-				DirectoryEntry ldapConnection = new DirectoryEntry(fullPath, Username, Password);
+				DirectoryEntry ldapConnection = new DirectoryEntry(fullPath);
+				ldapConnection.Username = Username;
+				ldapConnection.Password = Password;
 				ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
-				AddStackMessage("Successfully connected to:" + fullPath);
+				AddStackMessage("Successfully connected to:" + ldapConnection.Path);
 				return ldapConnection;
 			} catch(Exception e){
 				SetExceptionMessage(nameof(CreateDirectoryEntry), e.Message);
 				return new DirectoryEntry();	
 			}
-			
 		}
 
 		//will add the AD user into the passed local AD groups
-		public bool AssignLocalGroupsToUser(List<string> Groups){
+		public bool AssignGroupsToUser(List<string> Groups){
 			if(Groups != new List<string>()){
 				try{
 					using(PrincipalContext pc = new PrincipalContext(ContextType.Domain, _domainName)){
@@ -170,11 +175,11 @@ namespace SampleNamespace
 					return true;
 				}
 				catch(Exception e){
-					SetExceptionMessage(nameof(AssignLocalGroupsToUser), e.Message);
+					SetExceptionMessage(nameof(AssignGroupsToUser), e.Message);
 					return false;
 				}
 			} else{
-				SetExceptionMessage(nameof(AssignLocalGroupsToUser), "Groups is empty. Please pass a List<string> of the groups you want added");
+				SetExceptionMessage(nameof(AssignGroupsToUser), "Groups is empty. Please pass a List<string> of the groups you want added");
 				return false;
 			}
 		}
@@ -182,7 +187,7 @@ namespace SampleNamespace
 		//This should always be executed first in order to properly execute other functions
 		//verifies the admin credentials are valid by signing in and sets internal AD values
 		public bool Connect(string Username, string Password, string DomainName, string TLD){
-			AddStackMessage(nameof(Connect) + " has been initialized. Resetting _baseOU");
+			AddStackMessage(nameof(Connect) + " attempting to connect.");
 			_baseOU = new DirectoryEntry();
 			try{
 				AddStackMessage("Attempting to authenticate into: " + DomainName + "." + TLD);
@@ -201,27 +206,25 @@ namespace SampleNamespace
 			}
 		}
 
-		//E.G: if you want to create a user in OU Domain.COM/America/Washington/Department/Sales/Users
+		//E.G: if you want to create a user in an OU Domain.COM/America/Washington/Department/Sales/Users
 		//then OUList should be a List<string> with values: ["America", "Washington", "Department", "Sales", "Users"]
 		//points the internal _userEntry to the specified User OU
-		public bool ChangeOU(List<string> OUList){
+		public bool ChangeOU(string Username, string Password, List<string> OUList){
 			int totalOUCount = OUList.Count;
 			if(totalOUCount <= 0){
 				SetExceptionMessage(nameof(ChangeOU), "Not enough arguments in OUList, please make sure you have at least 1 item in the list");
 				return false;
 			}
-			try{
-				_userOU = _baseOU;
-				for(int current = 0; current < totalOUCount; current++){
-					_userOU = _userOU.Children.Add("OU=" + OUList[current], "CN");
-				}
-				return true;
-			} catch(Exception e){
-				SetExceptionMessage(nameof(ChangeOU), e.Message);
-				return false;
-			}
-		}
 
+			DirectoryEntries _tmpOU = _baseOU.Children;
+			for(int current=0; current < totalOUCount; current ++){
+				AddStackMessage("SEARCHING FOR NEXT OU:" + OUList[current]);
+				//_userOU = _tmpOU.Find("CN=" + OUList[current], _baseOU.SchemaClassName);
+				_userOU = _tmpOU.Find("OU=" + OUList[current], "organizationalUnit");
+				_tmpOU = _userOU.Children;
+			}
+			return true;
+		}
 		/*
 		When you create the dictionary, pass along the each key pair value in the format of "Key Name": "KeyValue"
 		REQUIRED PARAMETERS:
@@ -259,7 +262,7 @@ namespace SampleNamespace
 		//sets an exception method and returns false if no
 		private bool UserExists(string MethodName){
 			if(_user != new DirectoryEntry() && _user != _userOU){
-				AddStackMessage("User Instance exists under: " + MethodName);
+				AddStackMessage("User does exists. Called by: " + MethodName);
 				return true;
 			}
 			SetExceptionMessage(MethodName, "User instance not set: " + _user.Name + "--" + _user.Path);
@@ -301,7 +304,7 @@ namespace SampleNamespace
 				}
 				else if(kvp.Key == "Company"){
 					AddTabMessageToStack(kvp.Key, kvp.Value);
-					_user.Properties["co"].Value = kvp.Value;
+					_user.Properties["company"].Value = kvp.Value;
 				}
 				else if(kvp.Key == "Manager"){
 					AddTabMessageToStack(kvp.Key, kvp.Value);
@@ -358,13 +361,16 @@ namespace SampleNamespace
 					AddStackMessage("Updating ProfilePath to: " + kvp.Value);
 					_user.Properties["profilePath"].Value = kvp.Value;
 				}
-				else if(kvp.Key == "ScriptPath"){
+				else if(kvp.Key == "LogonScript"){
+					AddStackMessage("Updating LogonScript to: " + kvp.Value);
 					_user.Properties["scriptPath"].Value = kvp.Value;
 				}
 				else if(kvp.Key == "HomeDirectory"){
+					AddStackMessage("Updating HomeDirectory to: " + kvp.Value);
 					_user.Properties["homeDirectory"].Value = kvp.Value;
 				}
 				else if(kvp.Key == "HomeDrive"){
+					AddStackMessage("Updating HomeDrive to: " + kvp.Value);
 					_user.Properties["homeDrive"].Value = kvp.Value;
 				}
 			}
@@ -438,11 +444,9 @@ namespace SampleNamespace
 			}
 			foreach(var kvp in Parameters){
 				if(kvp.Key == "FirstName"){
-					_fullName = kvp.Value;
 					_user.Properties["givenName"].Value = kvp.Value;
 				}
 				else if(kvp.Key == "LastName"){
-					_fullName += " " + kvp.Value;
 					_user.Properties["sn"].Value = kvp.Value;
 				}
 				else if(kvp.Key == "DisplayName"){
@@ -475,9 +479,11 @@ namespace SampleNamespace
 		}
 
 		//determines if a path is valid or not
-		public bool ValidOU(string Path){
-			AddStackMessage("Verifying that the path is valid: "+ Path);
-			if(DirectoryEntry.Exists(Path)){
+		public bool ValidOU(){
+			AddStackMessage("Verifying that the path is valid: "+ _userOU.Path);
+			var exists = _userOU.Guid.ToString();
+			//if the GUID of the userOU exists then we assume that the OU is valid
+			if(exists != null || exists != ""){
 				AddStackMessage("Path is valid.");
 				return true;
 			}
@@ -489,11 +495,11 @@ namespace SampleNamespace
 		//if this returns false/exception than either the name is not valid or OU is not valid
 		public bool CreateADUser(string Name){
 			AddStackMessage("Creating AD User: " + Name);
-			if(ValidOU(_userOU.Path)){
-				_user = _userOU.Children.Add("CN=" + _fullName, "user");
+			if(ValidOU()){
+				_user = _userOU.Children.Add("CN=" + Name, "user");
 				_user.CommitChanges();
 				SetPassword(CreateSecurePassword(60));
-				AddStackMessage("AD User successfully created:" + _user.Path);
+				AddStackMessage("AD User successfully created at:" + _user.Path);
 				return true;
 			}
 			UserEntryException(nameof(CreateADUser));
@@ -502,38 +508,58 @@ namespace SampleNamespace
 
 		//use this if you want to be lazy and let the script handle everything(easiest way to use IMO)
 		//Creates the user and passes all parameter values into the UpdateTab() methods
-		public bool LazyCreateADUser(Dictionary<string, string> AllParameters){
+		public bool LazyCreateADUser(Dictionary<string, string> AllParameters, List<string> Groups){
 			AddStackMessage("Beginning Lazy AD Creation");
 			AddStackMessage("Checking that required parameters have been set");
+			bool upnFound = false, usernameFound = false, fullNameFound = false;
+			string fullName = "";
 			foreach(var kvp in AllParameters){
-				if(kvp.Key == "FirstName"){
+				if(kvp.Key == "FullName" && kvp.Value != ""){
+					fullName = kvp.Value;
 					AddStackMessage("First Name found");
-					_fullName = kvp.Value;
+					fullNameFound = true;
 				}
-				else if(kvp.Key == "LastName"){
-					AddStackMessage("Last Name found");
-					_fullName += " " + kvp.Value; 
-				}
-				else if(kvp.Key == "UPN"){
+				else if(kvp.Key == "UPN" && kvp.Value != ""){
 					AddStackMessage("UPN found");
+					upnFound = true;
 					_upn = kvp.Value;
 				}
-				else if(kvp.Key == "Username"){
+				else if(kvp.Key == "Username" && kvp.Value != ""){
 					AddStackMessage("Username found");
+					usernameFound = true;
 					_username = kvp.Value;
 				}
 			}
-			AddStackMessage("Validating Full Name: " + _fullName);
-			if(CreateADUser(_fullName)){
+			if(upnFound && usernameFound && fullNameFound && CreateADUser(fullName)){
 				bool account = UpdateAccount(AllParameters);
-				bool password = SetPassword(CreateSecurePassword(60));
-				bool general = UpdateGeneral(AllParameters);
-				bool address = UpdateAddress(AllParameters);
+				if(!account){
+					AddStackMessage(nameof(UpdateAccount) + " Failed to update Account Tab.");
+				}
 				bool profile = UpdateProfile(AllParameters);
+				if(!profile){
+					AddStackMessage(nameof(UpdateProfile) + " Failed to update Profile Tab.");
+				}
+				bool general = UpdateGeneral(AllParameters);
+				if(!general){
+					AddStackMessage(nameof(UpdateGeneral) + " Failed to update General Tab.");
+				}
+				bool address = UpdateAddress(AllParameters);
+				if(!address){
+					AddStackMessage(nameof(UpdateAddress) + " Failed to update Address Tab.");
+				}
 				bool org = UpdateOrganization(AllParameters);
-				if(account && password && general && address && profile && org){
+				if(!org){
+					AddStackMessage(nameof(UpdateAddress) + " Failed to update Organization Tab.");
+				}
+				bool groups = AssignGroupsToUser(Groups);
+				if(!groups){
+					AddStackMessage(nameof(AssignGroupsToUser) + " Failed to add user to group(s)");
+				}
+				if(account && general && address && profile && org && groups){
+					AddStackMessage("SUCESSFULLY CREATED USER: " + fullName + "-" + _upn);
 					return true;
 				}
+				SetExceptionMessage(nameof(LazyCreateADUser), "Failed to update values for user");
 				return false;
 			}
 			RequiredParametersException(nameof(LazyCreateADUser));
